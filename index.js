6ee08ddd0fda56a3fa5b8ad1bb36c20154937061
thomas.quick <thomas.quick@climate.com>
2019-10-12 00:44:46 -0500

    use ffprobes provided json output

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var ffprobe = module.exports = require('./lib/ffprobe.js');
+const ffprobe = module.exports = require('./lib/ffprobe.js')
 
-if(require.main === module) {
-	var exit = function(code, msg) {
-		process.nextTick(function() { process.exit(code) });
+if (require.main === module) {
+	const exit = function (code, msg) {
+		process.nextTick(() => process.exit(code))
 
-		if(code !== 0) console.error(msg);
-		else console.log(msg);
-	};
+		if (code !== 0) console.error(msg)
+		else console.log(msg)
+	}
 
-	var args = process.argv.slice(2);
+	const args = process.argv.slice(2)
 
-	if(args.length === 0) return exit(1, "Usage: node index.js /path/to/audio/file.mp3");
+	if (args.length === 0)
+		return exit(1, "Usage: node index.js /path/to/audio/file.mp3");
 
 	!function probeFile(file) {
-		if(!file) return exit(0, 'Finished probing all files');
-		file = '"'+file+'"';
-		ffprobe(file, function(err, results) {
-			console.log('%s\n========================================\n%s\n\n', file, err || JSON.stringify(results, null, '   '));
+		if (!file) return exit(0, 'Finished probing all files')
+		ffprobe(`"${file}"`, (err, results) => {
+			console.log('%s\n========================================\n%s\n\n', file, err || JSON.stringify(results, null, '   '))
 
-			probeFile(args.shift());
-		});
-	}(args.shift());
+			probeFile(args.shift())
+		})
+	}(args.shift())
 }
